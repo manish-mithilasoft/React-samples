589ce026876704a76d12a53fb90bc9c1f1775abc
manish jha <jhamanish@gmail.com>
2024-09-10 06:45:45 +0530

    few more examples

--- src/App.js
+++ src/App.js
@@ -1,38 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+//import { useState } from 'react';
 import React from 'react';
 
-function App() {
+function ProductCategoryRow({category})
+{
+    return(
+        <tr>
+            <th colSpan="2">{category}</th>
+        </tr>
+
+    );
+}
+function ProductRow({product}){
+    const name=product.stocked?product.name:
+    <span style={{ color: 'red' }}>
+    {product.name}
+  </span>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyButton/>
-        <MyButton/>
-      </header>
-    </div>
+
+    <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+    </tr>
   );
 }
-function MyButton() {
-  const[count,setCount]=useState(0);
-  function handleClick(){
-    setCount(count+1);
-    alert(`I am clicked ${count} times`);
+
+function ProductTable({products}){
+const rows=[];
+let lastCategory=null; 
+products.forEach(product => {
+    if(product.category!==lastCategory)
+    {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
+    }
+    
+        rows.push(<ProductRow product={product} key={product.name}/> );
+
+        lastCategory=product.category
+    
+}); 
+return (
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+{rows}
+        </tbody>
+    </table>
+); 
+}
+function SearchBar()
+{
+    return(
+        <form>
+            <input type='text' placeholder='Search Here'/>
+            <label>
+                <input type='checkbox' />{' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+function FilterableProductTable({products}){
+    return(
+        <div>
+            <SearchBar/>
+            <ProductTable products={products}/>
+        </div>
+    );
+}
+
+const PRODUCTS=[
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+  
+];
+export default function App() {
+    return <FilterableProductTable products={PRODUCTS} />;
   }
-  return (
-    <button onClick={handleClick}>I'm a button</button>
-  );
-}
-export default App;
